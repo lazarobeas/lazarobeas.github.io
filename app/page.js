f4a287c7e3cd5c1f614ed11538444f54172d401a
lazarobeas <39280428+lazarobeas@users.noreply.github.com>
2023-05-16 09:02:18 -0500

    Initial commit
    Portfolio Website v1

--- app/page.js
+++ app/page.js
@@ -1,113 +1,175 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import {BsFillMoonStarsFill, BsLinkedin, BsYoutube, BsTwitter, BsMedium} from "react-icons/bs";
+import {GoMarkGithub} from "react-icons/go";
+import headshotLBEAS from "../public/headshotLBEAS.jpg";
+import code from "../public/code.png";
+import design from "../public/design.png";
+import {SiHtml5, SiJavascript, SiCss3, SiReact, SiTailwindcss, SiBootstrap, SiPython, SiDjango, SiPandas, SiNumpy, SiTensorflow, SiJetbrains, SiScikitlearn, SiSelenium, SiCplusplus, SiGithub, SiNextdotjs, SiNodedotjs, SiDocker, SiYarn, SiGulp, SiGit, SiMongodb, SiSqlite, } from "react-icons/si";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-white px-10 md:px-20 lg:px-40">
+      <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+              <h1 className="text-xl font-burtons">🇲🇽 🇺🇸</h1>
+              <ul className="flex items-center">
+                  <li>
+                      <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+                  </li>
+                  <li>
+                      <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-3 py-2 rounded-xl ml-5"
+                          href="https://drive.google.com/file/d/1qUQXQmMUz1tfeo6I7bvzr6Bp4l1XoC8M/view?usp=share_link">
+                          Resume</a>
+                  </li>
+                  {/*<li>*/}
+                  {/*    <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-3 py-2 rounded-xl ml-5" href="#">Projects</a>*/}
+                  {/*</li>*/}
+              </ul>
+          </nav>
+          <div className="relative mx-auto rounded-full w-80 h-80 mt-20">
+              <Image src={headshotLBEAS}/>
+          </div>
+          <div className="text-center p-10">
+              <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Lazaro Beas</h2>
+              <h3 className="text-xl py-2 md:text-3xl">Software Engineer ● Full Stack Developer ● Student</h3>
+              <p className="text-md py-4 leading-8 text-gray-800 md:text-xl">
+                  Computer Science & Finance double major at the University of Texas Rio Grande Valley. My first love is
+                  Python but am currently learning C++ and R. I enjoy programming and am currently seeking an data science/software engineering internship
+                  or a freelance gig. I am a big fan of problem solving and love managing data. In my spare time,
+                  you can find me mountain biking, watching sports, reading, or looking at charts. Swain main for life.
+              </p>
+          </div>
+          <div className="text-5xl cursor-pointer flex justify-center gap-4 py-3 text-gray-600">
+              <a href="https://github.com/lazarobeas"><GoMarkGithub/></a>
+              <a href="https://www.linkedin.com/in/lazaro-beas-5ba702201/"><BsLinkedin/></a>
+              <a href="https://www.youtube.com/channel/UCt_ohKQSmycrTVWmqmuHcEA"><BsYoutube/></a>
+              <a href="https://medium.com/@lazarobeas2"><BsMedium/></a>
+          </div>
+      </section>
+
+      <section>
+          <div>
+              <h3 className="text-6xl text-teal-600 mt-10 text-center">Skills</h3>
+          </div>
+          <div className="lg:flex gap-5">
+              <div className="text-center shadow-lg p-10 rounded-xl my-1  dark:bg-white flex-1">
+                  <h3 className="text-4xl font-semibold pt-1 pb-6">
+                      Languages
+                  </h3>
+                  <div className="flex flex-wrap gap-15 text-3xl justify-evenly pb-10 mt-6">
+                      <p className="text-gray-800 px-10 py-5"><SiPython/>Python</p>
+                      <p className="text-gray-800 px-10 py-5"><SiJavascript/>Javascript</p>
+                      <p className="text-gray-800 px-10 py-5 "><SiCplusplus/>C++</p>
+
+                  </div>
+              <h4 className="text-md pt-4 text-center justify-self-center">
+                  My favorite languages for systems programming, software engineering, and data analysis.
+              </h4>
+              </div>
+              <div className="text-center shadow-lg p-10 rounded-xl my-1  dark:bg-white flex-1">
+                <h3 className="text-4xl font-semibold pt-1 pb-6">
+                    Front-End
+                </h3>
+                  <div className="flex flex-wrap gap-8 text-3xl justify-evenly mt-5">
+                    <p className="text-gray-800 px-1"><SiHtml5/>HTML5</p>
+                    <p className="text-gray-800 px-1"><SiCss3/>CSS</p>
+                    <p className="text-gray-800 px-1"><SiReact/>React</p>
+                    <p className="text-gray-800 px-1 py-1"><SiTailwindcss/>Tailwind CSS</p>
+                    <p className="text-gray-800 px-1 py-2"><SiBootstrap/>Bootstrap</p>
+                  </div>
+                  <h4 className="text-md pt-4 text-center justify-self-center">
+                      My preferred technologies for front-end web development and component design.
+                  </h4>
+              </div>
+              <div className="text-center shadow-lg p-10 rounded-xl my-1  dark:bg-white flex-1">
+                  <h3 className="text-4xl font-semibold pt-1 pb-6">
+                      Back-End
+                  </h3>
+                  <div className="flex flex-wrap gap-8 text-3xl justify-evenly mt-5">
+                      <p className="text-gray-800 px-1"><SiDjango/>Django</p>
+                      <p className="text-gray-800 px-1"><SiNodedotjs/>Node.js</p>
+                      <p className="text-gray-800 px-1"><SiGulp/>Gulp</p>
+                      <p className="text-gray-800 px-1"><SiMongodb/>MongoDB</p>
+                      <p className="text-gray-800 px-1"><SiPandas/>Pandas</p>
+                      <p className="text-gray-800 px-1"><SiScikitlearn/>Sci-kit</p>
+                      <p className="text-gray-800 px-1"><SiTensorflow/>TensorFlow</p>
+                      <p className="text-gray-800 py-1"><SiSelenium/>Selenium</p>
+                      <p className="text-gray-800 py-1"><SiNumpy/>Numpy</p>
+                  </div>
+                  <h4 className="text-md pt-4 text-center justify-self-center">
+                      My preferred technologies for back-end web programming, database architecture, and my favorite python packages.
+                  </h4>
+              </div>
+              <div className="text-center shadow-lg p-10 rounded-xl my-1  dark:bg-white flex-1">
+                  <h3 className="text-4xl font-semibold pt-1 pb-6">
+                      Tools
+                  </h3>
+                  <div className="flex flex-wrap gap-3 text-3xl justify-evenly">
+                      <p className="text-gray-800 px-10 py-5"><SiDocker/>Docker</p>
+                      <p className="text-gray-800 px-10 py-5"><SiGit/>Git</p>
+                      <p className="text-gray-800 px-10 py-5"><SiGithub/>Github</p>
+                      <p className="text-gray-800 px-10 py-5"><SiJetbrains/>Jetbrains</p>
+                  </div>
+                  <h4 className="text-md pt-4 text-center justify-self-center">
+                      My favorite tools for version control, code editing, and container orchestration.
+                  </h4>
+              </div>
+          </div>
+      </section>
+    <section>
+        <div>
+            <h3 className="text-6xl text-teal-600 mt-10 mb-10 text-center">Projects</h3>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className=" lg:flex flex-row text-right shadow-lg p-10 rounded-xl my-1  dark:bg-white flex-1">
+            <div className="flex-1 flex-wrap w-50 h-50">
+                <ul className="flex-2 flex-wrap">
+                    <li className="flex-5 flex-col-reverse px-12">
+                        <Image src={code}></Image>
+                        <h4 className="text-4xl font-semibold pt-1 pb-6 text-right">
+                            NBA Player Point Prediction Model
+                        </h4>
+                    </li>
+                    <li className="flex-3 flex-col text-right">
+                        <p>Using Python, Sci-kit, and Pandas, I developed a logistic
+                            regression model to predict whether an NBA player would score over x points in a given
+                            NBA game based on historical game data. The model accuracy ranged from 65%-33% with an
+                            average of 54% in a 1-month period. I also leveraged NBA-API to facilitate data collection and
+                            created my own dataset containing a player game splits for their entire career.
+                            I preprocessed data by creating dummy variables for new features such as home/away game advantage,
+                            and binarized the target variable based on a threshold of x points. Source code can be found
+                            <a href="https://github.com/lazarobeas/NBAProject"><span className="text-teal-600" src> here </span></a>or you can check my Github.
+                            You can also click the blue icon to try it out yourself!</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div className=" lg:flex flex-row text-right shadow-lg p-10 rounded-xl my-1  dark:bg-white flex-1">
+            <div className="flex-1 flex-wrap w-50 h-50">
+                <ul className="flex-2 flex-wrap">
+                    <div className="flex origin-right px-10">
+                    <Image src={design}></Image>
+                    </div>
+                    <li className="flex-5 flex-col text-right">
+                        <h4 className="text-4xl font-semibold pt-1 pb-6">
+                            FindADev Django Web App
+                        </h4>
+                    </li>
+                    <li className="flex-3 flex-col text-right">
+                        <p>Using Python, Sci-kit, and Pandas, I developed a logistic
+                            regression model to predict whether an NBA player would score over x points in a given
+                            NBA game based on historical game data. The model accuracy ranged from 65%-33% with an
+                            average of 54% in a 1-month period. I also leveraged NBA-API to facilitate data collection and
+                            created my own dataset containing a player game splits for their entire career.
+                            I preprocessed data by creating dummy variables for new features such as home/away game advantage,
+                            and binarized the target variable based on a threshold of x points. Source code can be found
+                            <a href="https://github.com/lazarobeas/NBAProject"><span className="text-teal-600" src> here </span></a>or you can check my Github.
+                            You can also click the blue icon to try it out yourself!</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: process.env.BASE_PATH,
+    assetPrefix: process.env.BASE_PATH,
+}
 
 module.exports = nextConfig
